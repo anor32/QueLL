ca2106f30e7cf0b1b13142c0a5a1d21a30f70448
anor23 <f@>
2024-12-27 15:16:06 +0300

    тесты для класса линкед лист написаны

--- LinkedListClass.py
+++ LinkedListClass.py
@@ -76,6 +76,7 @@ class LinkedList:
         while current_node:
             print(current_node.data)
             current_node = current_node.next_node
+
         return "Данные списка выведены"
 
     def get(self, data):
--- test_linked_list/test_linked_list.py
+++ test_linked_list/test_linked_list.py
@@ -28,7 +28,7 @@ def test_insert_end(linl):
     assert linl.insert_at_end(2) == None
     assert linl.head.data == 2
 
-    assert linl.insert_at_end("223") =="Узел с данными 223 добавлен в конец списка"
+    assert linl.insert_at_end("223") == "Узел с данными 223 добавлен в конец списка"
     assert linl.head.next_node.data == "223"
 
 
@@ -40,4 +40,33 @@ def test_remove_node(linl):
     assert linl.remove_node_position(2) == 'Удален узел с данными 1 позиции 2'
     assert linl.remove_node_position(10) == 'Ничего не удалено'
 
-def
+
+def test_insert_at_position(linl):
+    assert linl.insert_at_position("test_1", 1) == 'Узел с данными test_1 добавлен на позицию 1'
+    assert linl.insert_at_position("test_2", 2) == 'Узел с данными test_2 добавлен на позицию 2'
+    assert linl.insert_at_position("test_3", 5) == (None, 'не найдена позиция')
+
+
+def test_print_ll(linl):
+    linl.insert_at_head(1)
+    linl.insert_at_head(2)
+    linl.insert_at_head(3)
+
+    assert linl.print_ll() == "Данные списка выведены"
+
+
+def test_get(linl):
+    linl.insert_at_head("hello")
+    linl.insert_at_head("world")
+    linl.insert_at_head("python")
+
+    assert linl.get("world")[0] == True
+    assert linl.get("abracadabra")[0] == False
+
+def test_change_data(linl):
+    linl.insert_at_head("hello")
+    linl.insert_at_head("world")
+    linl.insert_at_head("python")
+    assert linl.change_data("python","abracadabra") =="Заменены данные в узле № 1"
+    assert linl.change_data("abracadabra","python") =="Заменены данные в узле № 1"
+    assert linl.change_data("wewe","abracadabra") =="Данные не обнаружены"
